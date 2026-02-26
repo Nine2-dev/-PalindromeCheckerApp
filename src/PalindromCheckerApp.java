2854c9b35943b66c31282d368338a14addf69bfa
Nine2-dev <navamijijuram@gmail.com>
2026-02-26 15:27:07 +0530

    UC2:Hardcoded palindrome

--- src/PalindromCheckerApp.java
+++ src/PalindromCheckerApp.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class PalindromCheckerApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get user input
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+
+        scanner.close();
+    }
 }
